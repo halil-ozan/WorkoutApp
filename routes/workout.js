bc68e333e8bcbc7a560898bbcc348c4824cbe5df
halilozan <halilozan@windowslive.com>
2024-12-02 22:09:03 +0300

    Initial commit - created backend

---
+++ routes/workout.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { 
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkout)
+
+//GET sing workouts
+router.get('/:id', getWorkouts)
+
+//POST a new workout
+router.post('/', createWorkout)
+
+//DELETE a new workout
+router.delete('/:id',deleteWorkout)
+
+//UPDATE a new workout
+router.patch('/:id',updateWorkout)
+
+module.exports = router
