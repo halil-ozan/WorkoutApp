676078473e27d6f4fca87a695a4b2bf4a44e0905
halilozan <halilozan@windowslive.com>
2025-01-15 20:06:53 +0300

    Authentication

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workout');
+const userRoutes = require('./routes/user');
 const { error } = require('console');
+
 
 //express app
 const app = express()
@@ -18,6 +20,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts',workoutRoutes)
+app.use('/api/user',userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
