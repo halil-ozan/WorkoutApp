ef80e3c1cc91f813a6d2e6de16e71c8fbb3b237c
halilozan <halilfurkanozan@hotmail.com>
2025-01-19 21:13:03 +0300

    auth final

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //pages & components
 import Home from './pages/Home'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -11,10 +13,18 @@ function App() {
       <Navbar />
         <div className='pages'>
           <Routes>
-            <Route
+          <Route
             path="/"
             element={<Home />}
             />
+          <Route
+            path="/login"
+            element={<Login/>}
+            />
+          <Route
+            path="/signup"
+            element={<Signup />}
+            />                        
           </Routes>
         </div>
       </BrowserRouter>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,38 @@
-
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
-  return(
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
     <header>
       <div className="container">
-        <Link to = "/">
-          <h1> Workout App</h1>
+        <Link to="/">
+          <h1>WorkoutBro</h1>
         </Link>
+        <nav>
+          {user &&(
+          <div>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          )}
+          {!user &&(
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+          )}
+        </nav>
       </div>
     </header>
   )
-
 }
 
-export default Navbar;
+export default Navbar
